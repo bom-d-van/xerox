f384f44662981bd4916c3bf4feb6b5ce36ec7c2c
bom_d_van <bom.d.van@gmail.com>
2014-01-30 13:05:21 +0800

    other stuff inited long ago

---
+++ generator.go
@@ -0,0 +1,5 @@
+package main
+
+var usage = `Xerox -t [package]
+	-t for test.
+`
---
+++ main_test.go
@@ -0,0 +1,23 @@
+package main
+
+// // @xerox
+// type Foo struct {
+// 	String string
+// 	Bar    *Bar
+// }
+
+// type Bar struct {
+// 	String string
+// }
+
+// func xeroxByLiteral(foo Foo) Foo {
+// 	return Foo{
+// 		String: foo.String,
+// 		Bar: &Bar{
+// 			String: foo.Bar.String,
+// 		},
+// 	}
+// }
+
+// func xeroxByReflect(foo Foo) Foo {
+// }
